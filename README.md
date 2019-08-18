ec85900aede99109eb28c05ecb6564ca2d20f550
LovelyWei <16489620+LovelyWei@users.noreply.github.com>
2019-08-18 21:41:33 +0800

    first

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # stage1stKeepLogin
- 
+## [泥潭](https://bbs.saraba1st.com/2b/forum.php) 在线挂机脚本
+
+# 使用方法
+Sept:
+- 登陆泥潭
+- 使用开发者工具找到Cookie `以Chrome为例 F12 -> Application -> Cookies`
+- 查看泥潭的Cookie 复制其中两项的值 *B7Y9_2132_auth,B7Y9_2132_saltkey*
+- 替换代码中Cookie相关的部分为你的 Cookie
+- 找一个vps 运行脚本 挂机
---
+++ stageKeepLogin.py
@@ -0,0 +1,27 @@
+import requests
+import time
+import random
+
+
+error=0
+while 1:
+    cookie = {
+    'B7Y9_2132_auth':'This is your auth',
+    'B7Y9_2132_saltkey':'This is your slatkey'
+}
+    try:
+        res = requests.get('https://bbs.saraba1st.com/2b/forum-75-1.html',cookies=cookie)
+        now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    except:
+        time.sleep(60)
+    if res.text.find('Aisina') != -1:
+        print('['+now+'] '+'Logined')
+        time.sleep(random.randint(120,480))
+        if error!=0:
+            error=0
+    else:
+        print('['+now+'] '+'Login failed!')
+        error=error+1
+        time.sleep(300)
+        if error>=3:
+            break
